ed50fe3939162714d901e47fe15b6b8d8fe3b48c
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-08-04 21:54:58 +0100

    Exercise 1.6 final

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,7 +8,20 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <div>
+        <h1>give feedback</h1>
+      </div>
+      <button onClick={() => { setGood(good + 1) }}>good</button>
+      <button onClick={() => { setNeutral(neutral + 1) }}>neutral</button>
+      <button onClick={() => { setBad(bad + 1) }}>bad</button>
+      <div>
+        <h1>statistics</h1>
+      </div>
+      <div>
+        <p style={{margin: '0px'}}>good {good}</p>
+        <p style={{margin: '0px'}}>neutral {neutral}</p>
+        <p style={{margin: '0px'}}>bad {bad}</p>
+      </div>
     </div>
   )
 }
