e27c9f8ba994aef92c1512e2699e8df879514046
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-08-09 22:30:51 +0100

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,51 +1,5 @@
 import React from "react";
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Total = ({ course }) => {
-  let sum = course.parts.reduce((a, b) => a + b.exercises, 0);
-  
-  return(
-    <p><b>Number of exercises {sum}</b></p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {
-        courses.map(course => 
-          (
-            <>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-          </>
-        ))
-      }
-    </div>
-  );
-};
+import Course from './components/Course.js'
 
 const App = () => {
   const courses = [
