7c7179cb75b18d91019b38bee36eafab827a7edb
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-08-14 13:20:04 +0100

    Exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,24 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Country = ({ country }) => (
+const Header = ({ name, capital, population }) => {
+  return (
+    <>
+      <h3>{name}</h3>
+      <p>capital {capital}</p>
+      <p>population {population}</p>
+    </>
+  );
+};
+
+const Languages = ({ languages }) => {
+  return (
+    <>
+      <h3>Spoken languages</h3>
+      <ul>
+        {languages.map((l) => (
+          <li key={l.iso639_1}>{l.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const Flag = ({ flag }) => (
+  <p>
+    <img
+      src={flag}
+      alt="flag"
+      style={{ width: "150px", height: "100px" }}
+    ></img>
+  </p>
+);
+
+const Weather = ({ weather }) => (
   <>
-    <h3>{country.name}</h3>
-    <p>capital {country.capital}</p>
-    <p>population {country.population}</p>
-    <h3>languages</h3>
-    <ul>
-      {country.languages.map((l) => (
-        <li key={l.iso639_1}>{l.name}</li>
-      ))}
-    </ul>
-    <p>
-      <img
-        src={country.flag}
-        alt="flag"
-        style={{ width: "150px", height: "100px" }}
-      ></img>
-    </p>
+    {weather ? (
+      <div>
+        <h3>Weather in {weather.location.name}</h3>
+        <b>temperature:</b> {weather.current.temperature} Celcius
+        <br/>
+        <img src={weather.current.weather_icons[0]} alt="weather"></img>
+        <br />
+        <b>wind:</b> {`${weather.current.wind_speed} mph direction ${weather.current.wind_dir}`}
+      </div>
+    ) : null}
   </>
 );
+
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [country]);
+  return (
+    <>
+      <Header
+        name={country.name}
+        capital={country.capital}
+        population={country.population}
+      />
+      <Languages languages={country.languages} />
+      <Flag flag={country.flag} />
+      <Weather weather={weather} />
+    </>
+  );
+};
 
 export default Country;
