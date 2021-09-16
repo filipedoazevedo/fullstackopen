cac06ae1610d53fb1cdd774e5efbc0c1f59bdae8
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-09-16 22:08:22 +0100

    Exercise 3.13

--- part3/index.js
+++ part3/index.js
@@ -1,70 +1,62 @@
 const express = require("express");
-const morgan = require('morgan');
-const path = require('path');
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
 
+require("dotenv").config();
+
+const Person = require(path.join(__dirname, "./models/person"));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens['body'](req, res),
-  ].join(' ')
-}));
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["body"](req, res),
+    ].join(" ");
+  })
+);
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    return res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
-  const info = `<div>
+  Person.find({}).then((persons) => {
+    const info = `<div>
                     <p>Phonebook has info for ${persons.length} people</p>
                     <p>${new Date()}</p>
                 </div>`;
-  res.send(info);
+    res.send(info);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
+  const id = req.params.id;
+  Person.findById(id)
+    .then((person) => {
+      if (person) return res.json(person);
 
-  if (person) {
-    return res.json(person);
-  }
-
-  return res.status(404).end();
+      return res.status(404).end();
+    })
+    .catch(() => res.status(404).end());
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -76,7 +68,7 @@ app.delete("/api/persons/:id", (req, res) => {
 
 app.post("/api/persons", (req, res) => {
   const { name, number } = req.body;
-  
+
   if (!name) {
     return res.status(400).json({
       error: "name is missing",
@@ -109,7 +101,7 @@ app.post("/api/persons", (req, res) => {
   return res.json(person);
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
