dd2e8ea85f64a2f7810a140a37068bbe5ab2bc78
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-10-07 21:59:17 +0100

    Exercise 3.19

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI;
 
@@ -14,9 +15,12 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { type: String, required: true, unique: true },
+  number: { type: String, required: true },
 });
+
+// Apply the uniqueValidator plugin to personSchema
+personSchema.plugin(uniqueValidator);
 
 const Person = mongoose.model("Person", personSchema);
 
