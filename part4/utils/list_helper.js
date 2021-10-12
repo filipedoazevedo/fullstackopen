27f2c02cea02fdad8ca5e43cf8aa2b11c51f551c
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-10-12 21:43:01 +0100

    Exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -12,7 +12,18 @@ const totalLikes = (blogs) => {
         : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (prev, item) => {
+        return (prev.likes > item.likes) ? prev : item
+    }
+
+    return blogs.length === 0
+        ? {}
+        : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
