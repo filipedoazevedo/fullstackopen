9b00124acd2b35111fdf1fc6802299cc489363e2
DESKTOP-KIT5JV1\Filipe <filipedani3@hotmail.com>
2021-10-15 21:16:15 +0100

    Exercise 4.8

--- part4/utils/logger.js
+++ part4/utils/logger.js
@@ -1,8 +1,12 @@
 const info = (...params) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params)
+    }
 }
 const error = (...params) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params)
+    }
 }
 module.exports = {
     info, error
